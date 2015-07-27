77c4576fc6b9a3d33ec991944aac622578d25c2c
reworkhow <reworkhow@gmail.com>
2015-07-26 23:40:45 -0500

    update check r^2

--- check_r2.sh
+++ check_r2.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+#######################################################
+#Author: Hao Cheng (haocheng@iastate.edu)
+#
+#script for single-trait single-step Bayesian analysis
+#
+#calculate accuracy for results from single-trait Bayes
+########################################################
+
+
 #check result
 ### breeding value in validation u_2
 ebv_nongeno=bw.pi975.out.50000.u_1
@@ -18,8 +29,6 @@ awk 'NR>1{print $3}' ebv.effect.temp > ebv.effect
 
 paste -d" " nongeno.geno.ID ebv.effect |sort> ebv.list
 
-
-
 ##get phenotypes in testinig dataset
 awk -F, 'NR!=1&&$8!="-999"{print $1,$5,$8}' aviagenData/tst_phens.csv|sort > phen.tst.list #id,fixed,pheno
 
@@ -29,21 +38,3 @@ join phen.tst.list ebv.list > $output
 join $output aviagenData/genotyped_TST.txt > $output".tst.geno"
 #nongenotyped test
 join -v1 $output aviagenData/genotyped_TST.txt > $output".tst.nongeno"
-
-
-#some fixed effect level are not estimateed in testing data
-#awk '{print $2}' phen.tst.list > fixed.test 
-#cgen_z -d fixed.test -e fixed.eff -r fixed.test -o X.test
-
-
-#awk '{print $1,$2}' phen.inp > phen.list
-#paste phen.list out1.u | sort> ebv.list
-
-#awk 'NR>1{print $1,$2}' bw.val | sort> val.list
-#join val.list ebv.list | awk '{print $1,$2,$4,$5}' > val.ebv
-#awk '{print $2,$3}' val.ebv > tmp
-#corr tmp > val.cor
-#rm tmp
-#rm Mt.sparse M.sparse
-#rm MtZtZMphi
-
